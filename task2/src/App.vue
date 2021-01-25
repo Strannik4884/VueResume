e9783ff360dcb5e076ea7fd779a63deacab01efa
Strannik4884 <strannik4884@mail.ru>
2021-01-25 08:56:10 +0300

    VueResume task2 init commit

---
+++ task2/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div class="form container" style="display: inline;">
+      <div class="d-flex">
+        <div class="left-col form-input col">
+          <resume-form :resume="resume"/>
+        </div>
+        <div class="right-col form-output col">
+          <resume-template :resume="resume"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ResumeForm from './components/ResumeForm.vue'
+import ResumeTemplate from './components/ResumeTemplate.vue'
+
+export default {
+  data() {
+    return {
+      // объект Resume с необходимыми полями
+      resume: {
+        profession: '',
+        city: '',
+        photoUrl: '',
+        name: '',
+        phone: '',
+        email: '',
+        birthday: '',
+        education: '',
+        desiredSalary: '',
+        skills: '',
+        about: ''
+      }
+    }
+  },
+  components: {
+    // компонент формы резюме
+    'resumeForm': ResumeForm,
+    // компонент шаблона резюме
+    'resumeTemplate': ResumeTemplate
+  }
+}
+</script>
+
+<style scoped>
+.left-col {
+  padding: 20px 0px 20px 100px;
+}
+
+.right-col {
+  padding: 20px 100px 20px 0px;
+}
+</style>
---
+++ task2/src/components/ResumeTemplate.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <h2 class="text-center form-col-title">Сформированное резюме</h2>
+    <div>
+      <div class="d-flex justify-content-center">
+        <!-- Если фотография не загружается, то отображаем стандартную фотографию для резюме -->
+        <img :src="resume.photoUrl" class="resume-image rounded-circle mb-2" alt="Image not found" onerror="this.src='person.jpg';">
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Профессия: {{ resume.profession }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Город: {{ resume.city }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">ФИО: {{ resume.name }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Номер телефона:: {{ resume.phone }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Email: {{ resume.email }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Дата рождения: {{ resume.birthday }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Образование: {{ resume.education }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Желаемая зарплата: {{ resume.desiredSalary }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">Навыки: {{ resume.skills }}</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="text-wrap mb-2" style="width: 100%">О себе: {{ resume.about }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResumeTemplate",
+  // переданный объект резюме
+  props:['resume'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-col-title {
+  padding-bottom: 20px;
+}
+.resume-image {
+  max-width: 250px;
+  max-height: 250px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+}
+</style>
